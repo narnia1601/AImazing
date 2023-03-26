9309815a8d081db1ce35559448c949d4d1f65c8c
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2023-03-26 16:19:05 +0800

    chart is created after user enters starting date and ending date

--- frontend/src/components/Chart.vue
+++ frontend/src/components/Chart.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <canvas id="myChart"></canvas>
-  <!-- <div v-else>Please enter a date range to get past finances</div> -->
+  <canvas id="myChart" v-if="datasets[0]['data'].length > 0"></canvas>
+  <div v-else>Please enter a date range to get past finances</div>
 </template>
 
 <script>
     export default {
         props: ['data'],
         data() {
-            return {
+            return { 
                 labels: [],
                 datasets: [
                     {
@@ -26,15 +26,36 @@
                 ]
             }
         },
-        mounted() {
-            const ctx = document.getElementById('myChart');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                labels: this.labels,
-                datasets: this.datasets
-                }
-            });
+        watch: {
+            data(data){
+                console.log(data)
+                data.forEach(element => {
+                    var amount = element['amount']
+                    var date = element['date']
+                    var formattedDate = date.substring(0,10)
+                    var [year, month, day] = formattedDate.split('-')
+                    var date = new Date(year, month - 1, day)
+                    var formattedDate = date.toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })
+                    this.labels.push(formattedDate)
+                    this.datasets[0]['data'].push(amount)
+                    console.log(this.datasets[0]['data'])
+                });
+                setTimeout(() => {
+                    this.buildChart()
+                }, 500);
+            }
+        },
+        methods: {
+            buildChart(){
+                const ctx = document.getElementById('myChart');
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                    labels: this.labels,
+                    datasets: this.datasets
+                    }
+                });
+            }
         },
     }
 </script>
