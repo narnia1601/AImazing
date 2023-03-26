2fc4c59c1df5b6eaaff59ac9f61bd26399f9293b
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2023-03-26 13:07:03 +0800

    created backend and frontend

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import Chart from 'chart.js/auto'
+</script>
+
+<template>
+  <canvas id="myChart"></canvas>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Finbro App</a>
+    </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
